c0c8e29a008e8c02abfe4cb9857b0bdd50d2595c
kovaleff <kovalev.v.u@gmail.com>
2026-04-10 13:07:27 +0300

    debug removed

--- app/Http/Controllers/MeditationController.php
+++ app/Http/Controllers/MeditationController.php
@@ -9,7 +9,6 @@ use Illuminate\Support\Facades\Log;
 class MeditationController extends Controller
 {
     function index(){
-        Log::debug('test');
         $meditations = Meditation::query()->orderBy('order')->get();
         return view('meditations', ['meditations' => $meditations]);
     }
